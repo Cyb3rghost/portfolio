70946d72ed03d3e6dfe24b5bb7cd32d7ed63f869
Cyb3rghost <cyb3rghostx@gmail.com>
2020-07-22 19:13:26 +0400

    Portfolio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,305 @@
+@extends('layout.layout')
+
+@section('contenu')
+
+<div class="container-fluid p-0">
+    <!-- About-->
+    <section class="resume-section" id="about">
+        <div class="resume-section-content">
+            <h1 class="mb-0">
+                Ludovic
+                <span class="text-primary">LEVENEUR</span>
+            </h1>
+            <div class="subheading mb-5">
+                56 BIS CHEMIN DU RUISSEAU · 97421 LA RIVIERE SAINT-LOUIS · 0692-72-93-22
+                <a href="mailto:ludovic.lvnr@gmail.com">ludovic.lvnr@gmail.com</a>
+            </div>
+            <p class="lead mb-5">Passionné par le web, je me suis plongé en autodidacte dans l'univers de la programmation. 
+                Ma tendance naturelle à me confronter à diverses technologies et mes nombreux projets font de moi un développeur polyvalent. 
+                Je possède aujourd'hui les capacités nécessaires à la réalisation d'applications web responsive pouvant répondre à une multitude de besoins. 
+                Je recherche des projets stimulants afin de me perfectionner et de m'épanouir dans un environnement enrichissant.</p>
+            <div class="social-icons">
+                <a class="social-icon" href="https://www.linkedin.com/in/ludovic-leveneur-26117817b/"><i class="fab fa-linkedin-in"></i></a>
+                <a class="social-icon" href="https://github.com/Cyb3rghost"><i class="fab fa-github"></i></a>
+                <a class="btn btn-danger" href="https://ludovic-leveneur.re/fichiers/CV%20LEVENEUR%20Ludovic%20-%20Dev%20Web%20(17012020).pdf" role="button">Curriculum vitæ</a><br/>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Experience-->
+    <section class="resume-section" id="experience">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Experience</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">CCAS Saint-Louis</h3>
+                    <div class="subheading mb-3">Développeur web en alternance</div>
+                    <p>Gestion de projet, développeur full-stack, conseils techniques, gestion hébergeur.</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Mars 2020 - A présent</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Sunriver</h3>
+                    <div class="subheading mb-3">Hôte et développeur à mon compte</div>
+                    <p>Gestion location saisonnière, création du projet, check-in, check-out, accueil des
+                        clients, création du site internet, création de mon système de réservation,
+                        prospection client, gestion offre promotionnelle, gestion publicité.
+                        </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Avril 2019 - Janvier 2020</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Gesa</h3>
+                    <div class="subheading mb-3">Technicien informatique</div>
+                    <p>Conseil client, montage et démontage pc fixe et portable,
+                        installation caisse et maintenance, installation point d’accès,
+                        installation et configuration imprimante réseau et usb, installation
+                        logiciel sage comptabilité et gestion, développement web
+                        application spécifique (simple).
+                        </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Novembre 2012 - Décembre 2016</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Delbos Informatique</h3>
+                    <div class="subheading mb-3">Stagiaire Technicien informatique</div>
+                    <p>Montage à la chaine pc fixe / Résolution divers / problème
+                        technique & logistique sur pc / Gestion SAV / Maintenance
+                        informatique réseau d’école primaire / Conseil achat client.
+                        </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Juin - Juillet 2010</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">PC Univers</h3>
+                    <div class="subheading mb-3">Stagiaire Technicien informatique</div>
+                    <p>Archivages, Démonter/ remonter pc fixe / Résolution divers
+                        problèmes technique & logistique sur pc.
+                        </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Mars - Avril 2009</span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Education-->
+    <section class="resume-section" id="education">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Formation</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Formation Simplon</h3>
+                    <div class="subheading mb-3">Saint-Denis</div>
+                    <div>Développement web / web mobile.</div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Mars 2020 - A présent</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Formation KapNumerik</h3>
+                    <div class="subheading mb-3">Saint-Pierre</div>
+                    <div>Développement application mobile (android / iOs).</div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Septembre 2018 - Février 2019</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Formation autodidacte online</h3>
+                    <div></div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2012-2018</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Lycée Paul Langevin</h3>
+                    <div class="subheading mb-3">Saint-Joseph</div>
+                    <div>Bac Pro Télécommunication et Réseau</div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2011</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Collège joseph Hubert</h3>
+                    <div class="subheading mb-3">Saint-Joseph</div>
+                    <div>Brevet des collèges
+                    </div>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">2008</span></div>
+            </div>
+            
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Skills-->
+    <section class="resume-section" id="skills">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Skills</h2>
+            <div class="subheading mb-3">Languages de programmation</div>
+            <ul class="list-inline dev-icons">
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/html-5.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/css3.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/javascript.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/100/000000/jquery.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/dusk/100/000000/php-logo.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/bootstrap.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/ios/100/000000/laravel.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/wordpress.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/officel/100/000000/react.png"></li>
+
+            </ul>
+            <div class="subheading mb-3">Outils</div>
+            <ul class="list-inline dev-icons">
+                <li class="list-inline-item"><img src="https://img.icons8.com/ios-filled/100/000000/github.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/trello.png"></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/nodejs.png"/></li>
+                <li class="list-inline-item"><img src="https://img.icons8.com/color/100/000000/npm.png"/></li>
+            </ul>
+            <!--<div class="subheading mb-3">Workflow</div>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Mobile-First, Responsive Design
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Cross Browser Testing & Debugging
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Cross Functional Teams
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Agile Development & Scrum
+                </li>
+            </ul>-->
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Interests-->
+    <section class="resume-section" id="projects">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Projets</h2>
+
+            <div class="row">
+
+                <div class="col-md-4">
+
+                    <div class="card" style="width: 18rem;">
+                        <a href="https://ludovic-leveneur.re/images/01.jpg"><img src="https://ludovic-leveneur.re/images/01.jpg" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                          <h5 class="card-title">Sunriver - Location saisonnière / Wordpress / DIVI</h5>
+                          <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                        </div>
+                        <a href="https://demo01.ludovic-leveneur.re/" class="btn btn-primary btn-block">Voir la démo</a>
+
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+
+                    <div class="card" style="width: 18rem;">
+                        <a href="https://ludovic-leveneur.re/images/02.jpg"><img src="https://ludovic-leveneur.re/images/02.jpg" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                          <h5 class="card-title">App. web "Réservation" / Fram. CSS Materialize / Fram. Laravel</h5>
+                          <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                        </div>
+                        <a href="https://demo02.ludovic-leveneur.re/" class="btn btn-primary btn-block">Voir la démo</a>
+                    </div>
+                    
+                </div>
+                <div class="col-md-4">
+
+                    <div class="card" style="width: 18rem;">
+                        <a href="https://ludovic-leveneur.re/images/03.jpg"><img src="https://ludovic-leveneur.re/images/03.jpg" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                          <h5 class="card-title">Calc. taxe de séjour / Fram. CSS Materialize / Fram. Laravel</h5>
+                          <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                        </div>
+                        <a href="https://demo03.ludovic-leveneur.re/" class="btn btn-primary btn-block">Voir la démo</a>
+                    </div><br/>
+                    
+                </div>
+
+                <br/>
+
+                <div class="col-md-4">
+
+                    <div class="card" style="width: 18rem;">
+                        <a href="https://ludovic-leveneur.re/images/04.jpg"><img src="https://ludovic-leveneur.re/images/04.jpg" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                          <h5 class="card-title">Gest. ordinateurs centre culturel / Fram. CSS Bootstrap / Fram. Laravel</h5>
+                          <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                        </div>
+                        <a href="https://demo04.ludovic-leveneur.re/" class="btn btn-primary btn-block">Voir la démo</a>
+                    </div>
+                    
+                </div>
+
+                <div class="col-md-4">
+
+                    <div class="card" style="width: 18rem;">
+                        <a href="https://ludovic-leveneur.re/images/todolist.jpg"><img src="https://ludovic-leveneur.re/images/todolist.jpg" class="card-img-top" alt="..."></a>
+                        <div class="card-body">
+                          <h5 class="card-title">ToDoList / HTML / CSS / BOOTSTRAP / JS / JQUERY / SQLITE</h5>
+                          <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+                        </div>
+                        <a href="https://github.com/Cyb3rghost/simplonTDL" class="btn btn-primary btn-block">Voir la démo</a>
+
+                    </div>
+                    
+                </div>
+
+
+                
+
+
+                
+
+
+
+            </div>
+
+
+
+            
+
+
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Interests-->
+    <section class="resume-section" id="interests">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Intérêts</h2>
+            <p class="text-justify"><b>L'entreprenariat & le monde des starts-up : </b> J'aime bien le monde de l'entreprenariat, le fait de croire en quelque chose,
+            d'offrir son temps et tous son énergie pour une seule et unique conviction de réussir et d'avoir le résultat qu'on souhaite atteindre. Ce monde me fascine, 
+            d'une part pour son côté inaccessible et d'une autre part pour son côté gratifiant sur le fait de participer à un projet motivant, utile aux autres et surtout
+            innovant.</p>
+            <p class="text-justify"><b>Les monnaies virtuelles, la crypto-monnaie, l'économie collaborative :</b> L'économie actuel ne me plaît pas, beaucoup d'inégalité,
+            beaucoup d'injustice et de corruption. J'observe de temps à autre toutes les nouveautés qui apparaissent dans le monde de la crypto-monnaie qui pourrait à mon 
+            sens changer les choses un jour très proche. Je n'aime cependant pas le côté énergivore de la crypto et non respectueux de l'environnement c'est pour cela 
+            que je n'ai pas totalement accroché. Cependant une monnaie virtuelle que j'aime bien c'est la June qui permet d'offrir un dividente universel à part égale 
+            à tous les participants. Ma vision : L'argent ne devrait pas être une contrainte mais un accomplissement car tout le monde à quelque chose à apporté et 
+            devrait faire ce qu'il aime. (Le sujet est vaste, les avis divergent c'est compliqué de ce faire une idée précise de tous sa à l'heure actuel.)</p>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Awards-->
+    <section class="resume-section" id="documents">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Documents</h2>
+
+            <a class="btn btn-danger" href="https://ludovic-leveneur.re/fichiers/CV%20LEVENEUR%20Ludovic%20-%20Dev%20Web%20(17012020).pdf" role="button">Curriculum vitæ</a><br/>
+            <br/>
+            <h3>L'architecte de votre vie numérique</h3>
+
+        </div>
+    </section>
+</div>
+
+@endsection('contenu')
